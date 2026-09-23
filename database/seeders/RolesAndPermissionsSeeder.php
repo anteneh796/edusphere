ff8c2869430e55c9592d3c844d391d8541a6cd00
anteneh796 <anteneh796@gmail.com>
2026-09-23 12:16:59 +0300

    Remove finance RBAC

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -10,6 +10,9 @@ class RolesAndPermissionsSeeder extends Seeder
 {
     public function run(): void
     {
+        Role::where('name', 'finance_officer')->delete();
+        Permission::where('name', 'like', 'finance.%')->delete();
+
         $matrix = config('rbac.roles');
         $labels = config('rbac.role_labels');
 
