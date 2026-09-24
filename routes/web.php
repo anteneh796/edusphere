73274a89355c612b52b619599b052f78e48293e6
anteneh796 <anteneh796@gmail.com>
2026-09-24 12:29:14 +0300

    Sprint 5: finish parent route paths

--- routes/web.php
+++ routes/web.php
@@ -457,7 +457,7 @@ Route::middleware(['auth', 'active', 'force.password'])->group(function () {
 
         Route::middleware(['permission:admissions.create'])->group(function () {
             Route::post('admissions/applications/{application}/parents', [ApplicationGuardianController::class, 'store'])->name('parents.store');
-            Route::put('admissions/applications/{application}/guardians/{guardian}', [ApplicationGuardianController::class, 'update'])->name('parents.update');
+            Route::put('admissions/applications/{application}/parents/{guardian}', [ApplicationGuardianController::class, 'update'])->name('parents.update');
             Route::delete('admissions/applications/{application}/guardians/{guardian}', [ApplicationGuardianController::class, 'destroy'])->name('parents.destroy');
             Route::post('admissions/applications/{application}/documents', [ApplicantDocumentController::class, 'store'])->name('documents.store');
             Route::post('admissions/applications/{application}/communications', [AdmissionCommunicationController::class, 'store'])->name('communications.store');
