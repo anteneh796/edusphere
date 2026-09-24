c7408bc6e3cb0184fe00333a43e037b9a15a50c5
anteneh796 <anteneh796@gmail.com>
2026-09-24 12:32:33 +0300

    Sprint 5: bind parent profile to existing family table

--- app/Domains/Students/Models/ParentProfile.php
+++ app/Domains/Students/Models/ParentProfile.php
@@ -6,6 +6,8 @@ use App\Support\Enums\ParentRelationship;
 
 class ParentProfile extends Guardian
 {
+    protected $table = 'guardians';
+
     public function relationshipLabel(): string
     {
         return ParentRelationship::tryFrom($this->relationship)?->label()
